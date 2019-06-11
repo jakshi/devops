9e57b6372badd9796b9c200c8d1b222c067c43a4
Kostiantyn Lysenko <kostiantyn@ddproperty.com>
2019-06-11 22:18:27 +0700

    Fix: installing hugo from package - adding dependency

--- scripts/install_hugo.sh
+++ scripts/install_hugo.sh
@@ -2,9 +2,11 @@
 
 HUGO_VERSION=0.55.6
 
-wget https://github.com/gohugoio/hugo/releases/download/v"$HUGO_VERSION"/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz -O /tmp/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
+# Dependency needed for hugo extended on travis
+wget -q -O libstdc++6 http://security.ubuntu.com/ubuntu/pool/main/g/gcc-5/libstdc++6_5.4.0-6ubuntu1~16.04.10_amd64.deb
+sudo dpkg --force-all -i libstdc++6
 
-tar -xzvf /tmp/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
+# And installing hugo itself
 
-suod mkdir -p /usr/local/bin
-sudo mv hugo /usr/local/bin
+wget -q -O hugo.deb https://github.com/gohugoio/hugo/releases/download/v"$HUGO_VERSION"/hugo_extended_"$HUGO_VERSION"_Linux_64bit.deb
+sudo dpkg -i hugo.deb

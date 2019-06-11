f7f52fc2f0e400099e5f76752f9ed5b1870219b2
Kostiantyn Lysenko <kostiantyn@ddproperty.com>
2019-06-11 21:50:28 +0700

    Re-facror: instead of building from scratch - intall hugo from he package

---
+++ scripts/install_hugo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+HUGO_VERSION=0.55.6
+
+wget https://github.com/gohugoio/hugo/releases/download/v"$HUGO_VERSION"/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz -O /tmp/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
+
+tar -xzvf /tmp/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
+
+mkdir -p /usr/local/bin
+mv hugo /usr/local/bin
